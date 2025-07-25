c8728efe52f3521d9aa5012859dd8adb048f9a5e
Abdelrahman Samy <abdelrahmansamy522@gmail.com>
2025-07-25 09:11:04 +0300

    Initialize Project

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import bcryptjs from "bcryptjs";
+import crypto from "crypto";
+
+
+
+export const signup = async (req, res) => {
+    res.send("Signup endpoint is not implemented yet.");
+};
+
+export const verifyEmail = async (req, res) => {
+    res.send("Verify email endpoint is not implemented yet.");
+};
+
+export const login = async (req, res) => {
+    res.send("Login endpoint is not implemented yet.");
+};
+
+export const logout = async (req, res) => {
+    res.send("Logout endpoint is not implemented yet.");
+};
+
+export const forgotPassword = async (req, res) => {
+    res.send("Forgot password endpoint is not implemented yet.");
+};
+
+export const resetPassword = async (req, res) => {
+    res.send("Reset password endpoint is not implemented yet.");
+};
+
+export const checkAuth = async (req, res) => {
+    res.send("Check auth endpoint is not implemented yet.");
+};
---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import { verifyToken } from "../middlewares/auth.middleware.js";
+import {
+    signup,
+    login,
+    logout,
+    checkAuth,
+    verifyEmail,
+    forgotPassword,
+    resetPassword
+} from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.get("/check-auth", checkAuth); // implement verifyToken middleware in the controller
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+
+
+export default router;
